3e0267a217e48c588071ef805164945f347c6806
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-05 13:10:10 +0200

    Add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import coursesReducer from './coursesRedux';
+// import loginReducer from './loginRedux';
+import { initialState } from './initialState';
+
+// define reducers
+const reducers = {
+  // courses: coursesReducer,
+  // isLogged: loginReducer,
+};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+const combinedReducers = combineReducers(reducers);
+
+// create store
+const store = createStore(
+  combinedReducers,
+  initialState,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+);
+
+export { store };
