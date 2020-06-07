b1dfc4c2da965e95286425afdbd33dfcd364f6b7
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-07 14:37:47 +0200

    Render CourseCard with data from state

--- src/components/common/CourseCard/CourseCard.js
+++ src/components/common/CourseCard/CourseCard.js
@@ -9,20 +9,24 @@ import { Link } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
-const Component = ({ className, children }) => (
-  <Link to={`${process.env.PUBLIC_URL}/`} className={clsx(className, styles.root)}>
-    <img src="img/bandage.jpg" alt="" className={styles.image} />
-    <div className={styles.description}>
-      <h2 className={styles.title}>Kurs VIDEO: Opatrunki specjalistyczne w aptece</h2>
-      <p className={styles.price}>299.00 PLN</p>
-      <button className={styles.button}>Dowiedz się więcej</button>
-    </div>
-  </Link>
-);
+const Component = ({ className, children, course }) => {
+  const { image, title, price } = course;
+  return (
+    <Link to={`${process.env.PUBLIC_URL}/`} className={clsx(className, styles.root)}>
+      <img src={`img/${image}`} alt={`Miniatura kursu: ${title}`} className={styles.image} />
+      <div className={styles.description}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.price}>{`${price}.00 PLN`}</p>
+        <button className={styles.button}>Dowiedz się więcej</button>
+      </div>
+    </Link>
+  );
+};
 
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  course: PropTypes.object,
 };
 
 // const mapStateToProps = state => ({
