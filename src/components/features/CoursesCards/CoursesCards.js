0d96533847952a76c7339d1606182f932a430405
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-07 16:14:56 +0200

    Refactor CaoursesCards as presentation comp

--- src/components/features/CoursesCards/CoursesCards.js
+++ src/components/features/CoursesCards/CoursesCards.js
@@ -6,46 +6,34 @@ import styles from './CoursesCards.module.scss';
 
 import { CourseCard } from '../../common/CourseCard/CourseCard';
 
+// import { connect } from 'react-redux';
+// import { getAll, fetchCourses } from '../../../redux/coursesRedux';
 
-import { connect } from 'react-redux';
-import { getAll, fetchCourses } from '../../../redux/coursesRedux';
+const Component = ({ courses, className }) => (
+  <section className={clsx(className, styles.root)} id="CourseCard">
+    {courses.map(course => <CourseCard key={course._id} course={course} />)}
+  </section>
+);
 
-class Component extends React.Component {
-  componentDidMount() {
-    const { fetchCourses } = this.props;
-    fetchCourses();
-  }
-
-  render() {
-    const { courses, className } = this.props;
-    return (
-      <section className={clsx(className, styles.root)} id="CourseCard">
-        {courses.map(course => <CourseCard key={course._id} course={course} />)}
-      </section>
-    );
-  }
-}
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
   courses: PropTypes.array,
-  fetchCourses: PropTypes.func,
 };
 
-const mapStateToProps = state => ({
-  courses: getAll(state),
-});
+// const mapStateToProps = state => ({
+//   courses: getAll(state),
+// });
 
-const mapDispatchToProps = dispatch => ({
-  fetchCourses: () => dispatch(fetchCourses()),
-});
+// const mapDispatchToProps = dispatch => ({
+//   fetchCourses: () => dispatch(fetchCourses()),
+// });
 
-const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  // Component as CoursesCards,
-  Container as CoursesCards,
+  Component as CoursesCards,
+  // Container as CoursesCards,
   Component as CoursesCardsComponent, //for tests
 };
 
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -7,35 +7,47 @@ import { Hero } from '../../features/Hero/Hero';
 import { CoursesCards } from '../../features/CoursesCards/CoursesCards';
 import { Newsletter } from '../../features/Newsletter/Newsletter';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { connect } from 'react-redux';
+import { getAll, fetchCourses } from '../../../redux/coursesRedux';
 
-const Component = ({ className, children }) => (
-  <main className={clsx(className, styles.root, 'container')}>
-    <Hero />
-    <CoursesCards />
-    <Newsletter />
-  </main>
-);
+class Component extends React.Component {
+  componentDidMount() {
+    const { fetchCourses } = this.props;
+    fetchCourses();
+  }
 
+  render() {
+    const { className, courses } = this.props;
+    return (
+      <main className={clsx(className, styles.root, 'container')
+      }>
+        <Hero />
+        <CoursesCards courses={courses} />
+        <Newsletter />
+      </main>
+    );
+  }
+}
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  courses: PropTypes.array,
+  fetchCourses: PropTypes.func,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  courses: getAll(state),
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  fetchCourses: () => dispatch(fetchCourses()),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as Homepage,
-  // Container as Homepage,
+  // Component as Homepage,
+  Container as Homepage,
   Component as HomepageComponent, //for tests
 };
 
