e47f6a66f5ae691f89feb1b0e23ddaa64b08e18e
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-07 16:59:03 +0200

    Add structure of Course comp (course details)

--- src/components/views/Course/Course.js
+++ src/components/views/Course/Course.js
@@ -4,34 +4,56 @@ import clsx from 'clsx';
 
 import styles from './Course.module.scss';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { connect } from 'react-redux';
+import { fetchCourseDetails } from '../../../redux/coursesRedux';
 
-const Component = ({ className, children }) => (
-  <main className={clsx(className, styles.root, 'container')}>
-    <h2>Course</h2>
-    {children}
-  </main>
-);
+class Component extends React.Component {
+  componentDidMount() {
+    const { fetchCourseDetails, match } = this.props;
+    fetchCourseDetails(match.params._id);
+  }
 
+  render() {
+    const { className, children, course } = this.props;
+    console.log(' : Component -> render -> course', course);
+    const { title, image, price, _id, chapters, length, gallery, description } = course;
+
+    return (
+      <main className={clsx(className, styles.root, 'container')}>
+        {title}
+        {image}
+        {price}
+        {_id}
+        {chapters}
+        {length}
+        {gallery}
+        {description}
+      </main>
+    );
+  }
+
+}
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  match: PropTypes.object,
+  course: PropTypes.object,
+  fetchCourseDetails: PropTypes.func,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  course: state.courses.currentCourse,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  fetchCourseDetails: id => dispatch(fetchCourseDetails(id)),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as Course,
-  // Container as Course,
+  // Component as Course,
+  Container as Course,
   Component as CourseComponent, //for tests
 };
 
