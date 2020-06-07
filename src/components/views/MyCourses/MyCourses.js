244344cbf38f670e5fb2d0d852d6cc8d037ab486
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-07 15:58:12 +0200

    Add restricted area to MyCourses component

--- src/components/views/MyCourses/MyCourses.js
+++ src/components/views/MyCourses/MyCourses.js
@@ -4,34 +4,43 @@ import clsx from 'clsx';
 
 import styles from './MyCourses.module.scss';
 
-// import { connect } from 'react-redux';
+import { Login } from '../Login/Login';
+
+import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
-const Component = ({ className, children }) => (
-  <main className={clsx(className, styles.root, 'container')}>
-    <h2>MyCourses</h2>
-    {children}
-  </main>
-);
+const Component = ({ className, children, isLogged }) => {
+
+  if (isLogged) {
+    return (
+      <main className={clsx(className, styles.root, 'container')}>
+        <h2>MyCourses</h2>
+        {children}
+      </main>
+    );
+  } else {
+    return <Login />;
+  }
+};
 
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  isLogged: PropTypes.bool,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  isLogged: state.isLogged,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as MyCourses,
-  // Container as MyCourses,
+  // Component as MyCourses,
+  Container as MyCourses,
   Component as MyCoursesComponent, //for tests
 };
 
