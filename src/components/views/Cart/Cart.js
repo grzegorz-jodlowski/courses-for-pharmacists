8fbe918a99d964e7e8ab805141f982280c52c9de
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-08 16:43:20 +0200

    Add button & cart conditional render if cart empty

--- src/components/views/Cart/Cart.js
+++ src/components/views/Cart/Cart.js
@@ -26,23 +26,26 @@ class Component extends React.Component {
   render() {
     const { className, children, cart } = this.props;
 
+
     return (<main className={clsx(className, styles.root, 'container')} >
       <h2 className={styles.title}>Zamówienie</h2>
 
-      {cart.map(({ quantity, courseId, title, price }) =>
-        <form key={courseId} className={styles.cartItem}>
-          <p className={styles.cartItemTitle}>{title}</p>
-          <input name="quantity" id="quantity" required className={styles.inputQuantity} type="number" value={quantity} onChange={this.handleQuantityChange.bind(this)} />
-          <textarea name="additionalInfo" id="additionalInfo" className={styles.additionalInfo} onChange={this.handleInfoChange.bind(this)} placeholder="Miejsce na dodatkowe informacje..."></textarea>
-          <button className={styles.removeButton} onClick={this.handleRemove.bind(this)}></button>
-        </form>
-
-      )}
-
-      <button className={styles.summaryButton}><Link to={`${process.env.PUBLIC_URL}/summary`} > Podsumuj zamówienie</Link></button>
-
-
-
+      {cart.length > 0
+        ?
+        <div>
+          {cart.map(({ quantity, courseId, title, price }) =>
+            <form key={courseId} className={styles.cartItem}>
+              <p className={styles.cartItemTitle}>{title}</p>
+              <input name="quantity" id="quantity" required className={styles.inputQuantity} type="number" value={quantity} onChange={this.handleQuantityChange.bind(this)} />
+              <textarea name="additionalInfo" id="additionalInfo" className={styles.additionalInfo} onChange={this.handleInfoChange.bind(this)} placeholder="Miejsce na dodatkowe informacje..."></textarea>
+              <button className={styles.removeButton} onClick={this.handleRemove.bind(this)}></button>
+            </form>
+          )}
+          <button className={styles.summaryButton}><Link to={`${process.env.PUBLIC_URL}/summary`} > Podsumuj zamówienie</Link></button>
+        </div>
+        :
+        <h2 className={styles.info}>Koszyk jest pusty</h2>
+      }
     </main>
     );
   }
