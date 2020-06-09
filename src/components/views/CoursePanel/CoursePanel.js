287706bb54e946e36651d60247e308080b7dcb73
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-09 11:31:32 +0200

    Add title of specific course in course panel

--- src/components/views/CoursePanel/CoursePanel.js
+++ src/components/views/CoursePanel/CoursePanel.js
@@ -5,35 +5,44 @@ import clsx from 'clsx';
 import styles from './CoursePanel.module.scss';
 
 import { Title } from '../../common/Title/Title';
+import { Spinner } from '../../common/Spinner/Spinner';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { connect } from 'react-redux';
 
-const Component = ({ className, children }) => (
-  <div className={clsx(className, styles.root, 'container')}>
-    <Title decoration={true} >Panel kursu DODAĆ TUTUŁ</Title>
-    {children}
-  </div>
-);
+const Component = ({ className, children, courses, match }) => {
+  const course = courses.find(course => course._id === match.params._id);
+
+  if (course) {
+    return (
+      <div className={clsx(className, styles.root, 'container')}>
+        <Title decoration={true} >{`Panel kursu: ${course.title}`}</Title>
+        {children}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+};
 
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  courses: PropTypes.array,
+  match: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  courses: state.courses.data,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as CoursePanel,
-  // Container as CoursePanel,
+  // Component as CoursePanel,
+  Container as CoursePanel,
   Component as CoursePanelComponent, //for tests
 };
 
