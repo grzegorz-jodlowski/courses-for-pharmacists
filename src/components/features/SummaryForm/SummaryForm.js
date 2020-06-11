2cf6458f3c108fd0c7e728518a39aef96d452011
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-11 13:22:59 +0200

    Add styles to SummaryForm

--- src/components/features/SummaryForm/SummaryForm.js
+++ src/components/features/SummaryForm/SummaryForm.js
@@ -7,6 +7,8 @@ import styles from './SummaryForm.module.scss';
 import { Title } from '../../common/Title/Title';
 import { Button } from '../../common/Button/Button';
 
+import { Link } from 'react-router-dom';
+
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
@@ -49,18 +51,27 @@ class Component extends React.Component {
 
   render() {
     const { handleSubmit, handleTermsChange, handlePrivacyChange, handleEmailChange, handleNameChange } = this;
-    const { className, children } = this.props;
+    const { className } = this.props;
+    const { name, email, privacy, terms } = this.state;
 
     return (
       <form className={clsx(className, styles.root)}>
         <label htmlFor="name">Imię <span>*</span></label>
-        <input name="name" id="name" required className={styles.inputName} type="text" value={this.state.name} onChange={handleNameChange.bind(this)} />
+        <input name="name" id="name" required className={styles.inputName} type="text" value={name} onChange={handleNameChange.bind(this)} />
         <label htmlFor="email">Email <span>*</span></label>
-        <input name="email" id="email" required className={styles.inputEmail} type="text" value={this.state.email} onChange={handleEmailChange.bind(this)} />
-        <label htmlFor="privacy">Privacy<span>*</span></label>
-        <input name="privacy" id="privacy" required className={styles.inputEmail} type="checkbox" value={this.state.privacy} onChange={handlePrivacyChange.bind(this)} />
-        <label htmlFor="terms">Terms<span>*</span></label>
-        <input name="terms" id="terms" required className={styles.inputEmail} type="checkbox" value={this.state.terms} onChange={handleTermsChange.bind(this)} />
+        <input name="email" id="email" required className={styles.inputEmail} type="text" value={email} onChange={handleEmailChange.bind(this)} />
+        <label htmlFor="privacy" className={styles.labelPrivacy}>
+          <input name="privacy" id="privacy" required className={styles.inputPrivacy} type="checkbox" value={privacy} onChange={handlePrivacyChange.bind(this)} />
+          <p>
+            Wyrażam zgodę na przetwarzanie moich danych osobowych w celach i zakresie zgodnym z<Link to={`${process.env.PUBLIC_URL}/privacy`} className={styles.link}>{' Polityką prywatności.'}</Link><span>*</span>
+          </p>
+        </label>
+        <label htmlFor="terms" className={styles.labelTerms}>
+          <input name="terms" id="terms" required className={styles.inputTerms} type="checkbox" value={terms} onChange={handleTermsChange.bind(this)} />
+          <p>
+            Akceptuję <Link to={`${process.env.PUBLIC_URL}/terms`} className={styles.link}>{' regulamin zakupów '}</Link><span>*</span>
+          </p>
+        </label>
         <Button action={handleSubmit.bind(this)} text={'Zamawiam i płacę'} path={'summary'} />
       </form>
     );
