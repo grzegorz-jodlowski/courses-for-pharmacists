c90a8429d9cda3e2e3d3ed572107ab544273c99b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-12 18:57:01 +0200

    Add validation to newsletter

--- src/components/features/Newsletter/Newsletter.js
+++ src/components/features/Newsletter/Newsletter.js
@@ -20,6 +20,7 @@ class Component extends React.Component {
     name: '',
     email: '',
     postError: false,
+    validationError: null,
     loading: false,
     success: false,
   };
@@ -34,40 +35,53 @@ class Component extends React.Component {
     const { name, email } = this.state;
 
     e.preventDefault();
-    this.setState({ loading: true });
 
+    let error = null;
 
-    Axios
-      .post(`${api.url}/${api.newsletter}`, { name, email })
-      .then(res => {
-        console.log(' : handleSubmit -> res.data', res.data);
-        this.setState({
-          name: '',
-          email: '',
-          postError: false,
-          loading: false,
-          success: true,
+    const emailPattern = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'g');
+
+    if (!name.length || !email.length) error = `Uzupełnij imię i email`;
+    else if (name.length > 50 || name.length < 5) error = `Imię może zawierać 5-50 znaków`;
+    else if (!emailPattern.test(email)) error = `Nieprawidłowy adres email`;
+
+    if (!error) {
+
+      this.setState({ loading: true });
+
+      Axios
+        .post(`${api.url}/${api.newsletter}`, { name, email })
+        .then(res => {
+          console.log(' : handleSubmit -> res.data', res.data);
+          this.setState({
+            name: '',
+            email: '',
+            postError: false,
+            validationError: null,
+            loading: false,
+            success: true,
+          });
+        })
+        .catch(err => {
+          console.log(' : handleSubmit -> err.message', err.message);
+          this.setState({
+            postError: true,
+            loading: false,
+            success: false,
+          });
         });
-      })
-      .catch(err => {
-        console.log(' : handleSubmit -> err.message', err.message);
-        this.setState({
-          postError: true,
-          loading: false,
-          success: false,
-        });
-      });
+    } else {
+      this.setState({ validationError: error });
+    }
   }
 
   render() {
     const { handleSubmit, handleChange } = this;
-    const { name, email, loading, postError, success } = this.state;
+    const { name, email, loading, postError, validationError, success } = this.state;
 
 
     return (
       <section className={clsx(styles.root)} >
         {(!loading && !postError && success) && <Info variant={'success'}>{`Zapiasno do newsletter`}</Info>}
-        {(postError) && <Info variant={'error'}>{'Ups... coś poszło nie tak!'}</Info>}
         {(loading) && <Spinner />}
         {(!loading && !success) &&
           (
@@ -79,6 +93,8 @@ class Component extends React.Component {
                 <label htmlFor="email">Email <span>*</span></label>
                 <input name="email" id="email" required className={styles.inputEmail} type="text" value={email} onChange={handleChange} />
                 <p className={styles.disclaimer} >Zapisując się na newsletter wyrażasz zgodę na przesyłanie informacji o produktach, nowościach i promocjach ze strony Kursy dla Farmaceutów.</p>
+                {(validationError) && <Info variant={'warning'}>{validationError}</Info>}
+                {(postError) && <Info variant={'error'}>{'Ups... coś poszło nie tak!'}</Info>}
                 <Button submitForm={true} text={'Zapisz mnie i wyślij rozdziały!'} />
               </form>
             </div>
