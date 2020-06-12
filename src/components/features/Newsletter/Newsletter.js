1ec96f3469163dea4ce9132c1df24e28265a06f9
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-12 18:40:15 +0200

    Add conditional rendering to newsletter comp

--- src/components/features/Newsletter/Newsletter.js
+++ src/components/features/Newsletter/Newsletter.js
@@ -9,6 +9,8 @@ import styles from './Newsletter.module.scss';
 
 import { Button } from '../../common/Button/Button';
 import { Title } from '../../common/Title/Title';
+import { Info } from '../../common/Info/Info';
+import { Spinner } from '../../common/Spinner/Spinner';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
@@ -17,10 +19,14 @@ class Component extends React.Component {
   state = {
     name: '',
     email: '',
+    postError: false,
+    loading: false,
+    success: false,
   };
 
   handleChange = ({ target }) => {
     const { name, value } = target;
+
     this.setState({ [name]: value });
   }
 
@@ -28,36 +34,56 @@ class Component extends React.Component {
     const { name, email } = this.state;
 
     e.preventDefault();
+    this.setState({ loading: true });
 
 
     Axios
       .post(`${api.url}/${api.newsletter}`, { name, email })
       .then(res => {
         console.log(' : handleSubmit -> res.data', res.data);
-        this.setState({ name: '', email: '' });
+        this.setState({
+          name: '',
+          email: '',
+          postError: false,
+          loading: false,
+          success: true,
+        });
       })
       .catch(err => {
         console.log(' : handleSubmit -> err.message', err.message);
+        this.setState({
+          postError: true,
+          loading: false,
+          success: false,
+        });
       });
   }
 
   render() {
     const { handleSubmit, handleChange } = this;
-    const { name, email } = this.state;
+    const { name, email, loading, postError, success } = this.state;
 
 
     return (
       <section className={clsx(styles.root)} >
-        <Title decoration={true}>Zapisz się na newsletter i odbierz darmowe rozdziały oferowanych kursów!</Title>
-
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <label htmlFor="name">Imię <span>*</span></label>
-          <input name="name" id="name" required className={styles.inputName} type="text" value={name} onChange={handleChange} />
-          <label htmlFor="email">Email <span>*</span></label>
-          <input name="email" id="email" required className={styles.inputEmail} type="text" value={email} onChange={handleChange} />
-          <p className={styles.disclaimer} >Zapisując się na newsletter wyrażasz zgodę na przesyłanie informacji o produktach, nowościach i promocjach ze strony Kursy dla Farmaceutów.</p>
-          <Button submitForm={true} text={'Zapisz mnie i wyślij rozdziały!'} />
-        </form>
+        {(!loading && !postError && success) && <Info variant={'success'}>{`Zapiasno do newsletter`}</Info>}
+        {(postError) && <Info variant={'error'}>{'Ups... coś poszło nie tak!'}</Info>}
+        {(loading) && <Spinner />}
+        {(!loading && !success) &&
+          (
+            <div>
+              <Title decoration={true}>Zapisz się na newsletter i odbierz darmowe rozdziały oferowanych kursów!</Title>
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <label htmlFor="name">Imię <span>*</span></label>
+                <input name="name" id="name" required className={styles.inputName} type="text" value={name} onChange={handleChange} />
+                <label htmlFor="email">Email <span>*</span></label>
+                <input name="email" id="email" required className={styles.inputEmail} type="text" value={email} onChange={handleChange} />
+                <p className={styles.disclaimer} >Zapisując się na newsletter wyrażasz zgodę na przesyłanie informacji o produktach, nowościach i promocjach ze strony Kursy dla Farmaceutów.</p>
+                <Button submitForm={true} text={'Zapisz mnie i wyślij rozdziały!'} />
+              </form>
+            </div>
+          )
+        }
       </section>
     );
   }
