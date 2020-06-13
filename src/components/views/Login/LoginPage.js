3a51908b2a5ca4ebfa7ee5de7921170460f92b7a
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-13 17:03:21 +0200

    Add login with google button & conditional render

--- src/components/views/Login/LoginPage.js
+++ src/components/views/Login/LoginPage.js
@@ -5,35 +5,46 @@ import clsx from 'clsx';
 import styles from './LoginPage.module.scss';
 
 import { Title } from '../../common/Title/Title';
+import { Info } from '../../common/Info/Info';
 
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
-const Component = ({ className, children }) => (
-  <main className={clsx(className, styles.root, 'container')}>
-    <Title decoration={true} >Logowanie</Title>
-    {children}
-  </main>
-);
+const Component = ({ className, children, isLogged }) => {
+  return (
+    <main className={clsx(className, styles.root, 'container')}>
+      {isLogged ?
+        <Info variant={'success'} >Jesteś zalogowany</Info>
+        :
+        <div>
+          <Title>Zaloguj się żeby uzyskać dostęp do swoich kursów</Title>
+          <a className={clsx(styles.loginBtn, styles.loginBtnGoogle)} href="/auth/google">
+            Login with Google
+          </a>
+        </div>
+      }
+    </main>
+  );
+};
 
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  isLogged: PropTypes.bool,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  isLogged: state.isLogged,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as LoginPage,
-  // Container as LoginPage,
+  // Component as LoginPage,
+  Container as LoginPage,
   Component as LoginPageComponent, //for tests
 };
 
