f1394452a1377c67561555c4762dc0ff85b2ff9d
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-15 22:02:16 +0200

    Refactor MainLayout comp (remove select for login)

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -7,17 +7,9 @@ import styles from './MainLayout.module.scss';
 import { PageNav } from '../PageNav/PageNav';
 import { Footer } from '../Footer/Footer';
 
-import { connect } from 'react-redux';
-import { updateLoginStatus } from '../../../redux/loginRedux';
-
-const Component = ({ className, children, updateLoginStatus }) => (
+const Component = ({ className, children }) => (
   <div className={clsx(className, styles.root)}>
     <PageNav />
-    {/* select for tests */}
-    <select onChange={(e) => updateLoginStatus(e.target.value)} className={styles.select}>
-      <option value="login">Login</option>
-      <option value="logout">Logout</option>
-    </select>
     {children}
     <Footer />
   </div>
@@ -26,22 +18,10 @@ const Component = ({ className, children, updateLoginStatus }) => (
 Component.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
-  updateLoginStatus: PropTypes.func,
-
 };
 
-const mapStateToProps = state => ({
-});
-
-const mapDispatchToProps = dispatch => ({
-  updateLoginStatus: log => dispatch(updateLoginStatus(log)),
-});
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
 export {
-  // Component as MainLayout,
-  Container as MainLayout,
+  Component as MainLayout,
   Component as MainLayoutComponent, //for tests
 };
 
