16576ca1850b68cfa56f1474153fff1d4dea95f2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-17 18:28:23 +0200

    Update styles for Search comp

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { register } from './serviceWorker';
-import { unregister } from './serviceWorker';
+import { register, unregister } from './serviceWorker';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
