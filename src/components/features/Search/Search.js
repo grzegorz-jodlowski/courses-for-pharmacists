4929b129bcd862c15b63649874f4ab7c435f058b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-17 16:42:34 +0200

    Update structure of Search component

--- src/components/features/Search/Search.js
+++ src/components/features/Search/Search.js
@@ -4,42 +4,80 @@ import clsx from 'clsx';
 
 import styles from './Search.module.scss';
 
-import { Title } from '../../common/Title/Title';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
+import { connect } from 'react-redux';
+import { changeSearchString } from '../../../redux/searchRedux';
 
 class Component extends React.Component {
+  state = {
+    value: this.props.searchString,
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+      visibleButtons: event.target.value.length > 0,
+    });
+  }
+
+  handleOK() {
+    this.props.changeSearchString(this.state.value);
+    // this.props.history.push(`/search/${this.state.value}`);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.searchString != prevProps.searchString) {
+      this.setState({ value: this.props.searchString });
+    }
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.handleOK();
+    }
+  }
+
 
   render() {
-    const { className } = this.props;
+    const { className, text } = this.props;
+    const { value } = this.state;
     return (
 
       <div className={clsx(className, styles.root)}>
-        <Title decoration={false}>Search</Title>
+        <input
+          type='text'
+          placeholder={text}
+          value={value}
+          onChange={event => this.handleChange(event)}
+          onKeyPress={this.handleKeyPress}
+        />
+        <div className={styles.buttons}>
+          <button onClick={() => this.handleOK()}>ok</button>
+        </div>
       </div>
     );
   }
 }
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  searchString: PropTypes.string,
+  text: PropTypes.string,
+  changeSearchString: PropTypes.func,
+  history: PropTypes.object,
 };
 
-// const mapStateToProps = state => ({
-//   concerts: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  searchString: state.searchString,
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  changeSearchString: newSearchString => dispatch(changeSearchString(newSearchString)),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as Search,
-  // Container as Search,
+  Container as Search,
   Component as SearchComponent, //for tests
 };
 
