b6824da6892337405a6bf8a2fb72013d7686946b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-18 16:46:46 +0200

    Update Contact component about functions

--- src/components/features/Contact/Contact.js
+++ src/components/features/Contact/Contact.js
@@ -9,12 +9,45 @@ import { Title } from '../../common/Title/Title';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/ExampleRedux';
 
-const Component = ({ className, children }) => (
-  <div className={clsx(className, styles.root)}>
-    <Title decoration={false}>Contact</Title>
-    {children}
-  </div>
-);
+class Component extends React.Component {
+  state = {
+    name: '',
+    email: '',
+    text: '',
+    postError: false,
+    // validationError: null,
+    // loading: false,
+    // success: false,
+  };
+  handleSubmit = (e) => {
+    const { name, email } = this.state;
+  }
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { handleSubmit, handleChange } = this;
+
+    const { className } = this.props;
+
+    return (
+      <div className={clsx(className, styles.root)}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" value={this.state.name} onChange={handleChange} />
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={handleChange} />
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" rows="5" value={this.state.message} onChange={handleChange} />
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 Component.propTypes = {
   children: PropTypes.node,
