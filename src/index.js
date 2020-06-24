9394f1437bd26c5e48c6339d0e2663cbd4ddea38
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-24 17:48:22 +0200

    Update app name and remove unregister import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { register, unregister } from './serviceWorker';
+import { register } from './serviceWorker';
 import './styles/normalize.scss';
 import './styles/global.scss';
 
